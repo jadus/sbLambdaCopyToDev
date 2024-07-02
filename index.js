b0d152c547ad24c447e9c2c4891bbab0b05445da
Lucas Ménant <lucas.menant@gmail.com>
2024-07-02 17:40:27 +0200

    première version

--- index.js
+++ index.js
@@ -1,41 +1,28 @@
-'use strict';
-
 const AWS = require('aws-sdk');
-const S3 = new AWS.S3({
-  signatureVersion: 'v4',
-});
-const Sharp = require('sharp');
+const s3 = new AWS.S3();
 
-
-const widths = [400, 800];
-
-const promises = [];
-
-exports.handler = function(event, context) {
-    const key = event.Records[0].s3.object.key;
-    const bucket = event.Records[0].s3.bucket.name;
-    if(key.substring(0, 13) == 'bouldersPics/') {
-
-        widths.forEach((width) => {
-            promises.push(
-                S3.getObject({Bucket: bucket, Key: key}).promise()
-                    .then(data => Sharp(data.Body)
-                        .resize(width)
-                        .toFormat('jpeg')
-                        .toBuffer()
-                    )
-                    .then(buffer => S3.putObject({
-                            Body: buffer,
-                            Bucket: bucket,
-                            ContentType: 'image/jpeg',
-                            Key: width.toString()+'/'+key,
-                        }).promise()
-                    )
-                    .catch(err => callback(err))
-            )
-
-        })
-
-        Promise.all(promises).then(()  => context.done());
+exports.handler = async (event) => {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    
+    // Get the bucket and object key from the event
+    const sourceBucket = event.Records[0].s3.bucket.name;
+    const sourceKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    
+    // Define the destination bucket
+    const destinationBucket = 'socialboulderdev';
+    
+    // Define the copy source
+    const copySource = `/${sourceBucket}/${sourceKey}`;
+    
+    try {
+        // Copy the object
+        await s3.copyObject({
+            CopySource: copySource,
+            Bucket: destinationBucket,
+            Key: sourceKey
+        }).promise();
+        
+    } catch (err) {
+        throw err;
     }
-}
+};
